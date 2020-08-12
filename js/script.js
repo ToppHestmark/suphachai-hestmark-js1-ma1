605788f95814d0ee4f6aae6e7e49dbaafc4ca691
Topp <48948074+ToppHestmark@users.noreply.github.com>
2020-08-12 09:07:26 +0200

    Update script.js
    6 questions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// const cats = [
-//     {
-//         name: "Blob",
-//         age: 10
-//     },
-//     {
-//         name: "Harold",
-//     },
-//     {
-//         name: "Blurt",
-//         age: 21
-//     }
-// ];
-
 // Question 1
 // Create an object called cat.
 // Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".
@@ -20,7 +6,7 @@ const cat = {
         return "Meow!";
     }
 }
-// console.log(cat.complain);
+console.log(cat.complain());
 
 // Question 2
 // Select the h3 from the HTML using the querySelector method and assign it to a variable called heading.
@@ -43,36 +29,27 @@ const paragraphs = document.querySelectorAll("p");
 // Loop through the p elements and change the colour of each to "red".
 for (let i = 0; i < paragraphs.length; i++) {
     paragraphs[i].style.color = "red";
-}
+};
 
 // Question 6
 // Select the div with a class of results, assign it to a variable called resultsContainer and set its inner HTML to be <>New paragraph</> and its background colour to be yellow.
-const resultsContainer = document.querySelector("results");
-const newParagraph = document.createElement("p")
-
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML += "<p>New paragraph</p>"
+resultsContainer.style = "background-color: yellow";
 
 // Question 7
 // Create a function that has one parameter called list.
-
 // Inside the function, loop through the list parameter and console log the name property in each object.
-
 // Call the function and pass in the cats variable in the script.js file in the repo.
 
 // Question 8
 // Create a function called createCats. The function will have one parameter called cats.
-
 // Inside the function loop through the value passed in as cats and create HTML for each object in the array.
-
 // Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
-
 // If the age property is missing, it should display Age unknown instead.
-
 // Return the HTML from the function.
-
 // Call the function and pass in the cats array as the argument.
-
 // Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.
-
 // The function should return the following:
 
 // <div>
@@ -87,4 +64,3 @@ const newParagraph = document.createElement("p")
 //     <h5>Blurt</h5>
 //     <p>21</p>
 // </div>
-
