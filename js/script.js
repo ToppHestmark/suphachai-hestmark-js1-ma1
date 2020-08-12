0ff026cb7b60dd824de0a4c60a9ef7f540d5947c
Topp <48948074+ToppHestmark@users.noreply.github.com>
2020-08-12 18:26:01 +0200

    Question 7
    Finished question 7

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
 // Question 1
 // Create an object called cat.
 // Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".
@@ -41,6 +55,12 @@ resultsContainer.style = "background-color: yellow";
 // Create a function that has one parameter called list.
 // Inside the function, loop through the list parameter and console log the name property in each object.
 // Call the function and pass in the cats variable in the script.js file in the repo.
+function listFunction(list) {
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i].name)
+    }
+}
+listFunction(cats);
 
 // Question 8
 // Create a function called createCats. The function will have one parameter called cats.
@@ -64,3 +84,4 @@ resultsContainer.style = "background-color: yellow";
 //     <h5>Blurt</h5>
 //     <p>21</p>
 // </div>
+
